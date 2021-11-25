1157553951bddb2f3071bc59d93a06028cf881b3
Hemanth-Bethu <78938009+Hemanth-Bethu@users.noreply.github.com>
2021-11-25 22:50:01 +1100

    Add files via upload

---
+++ Health-and-fitness/public/js/firebase.js
@@ -0,0 +1,28 @@
+let firebaseConfig = {
+    // enter your firebase credentials here.
+
+    
+    apiKey: "AIzaSyAt9inT83oZ4ibZm9sXA6is4LQuyXimfjI",
+    authDomain: "health-and-fitness-8a894.firebaseapp.com",
+    projectId: "health-and-fitness-8a894",
+    storageBucket: "health-and-fitness-8a894.appspot.com",
+    messagingSenderId: "998642422337",
+    appId: "1:998642422337:web:b07299b95fba11da07acc0",
+    measurementId: "G-CDG3VW0QDG"
+  
+
+ 
+
+
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+let auth = firebase.auth();
+
+const logoutUser = () => {
+    auth.signOut();
+    location.reload();
+}
